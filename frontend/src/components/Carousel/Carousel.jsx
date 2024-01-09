363c501d6f16300635bcf33cd862fa6bcbb17610
Sandra Gustafsson <sandragustafsson@Mac-mini.local>
2024-01-09 18:39:29 +0100

    new branch

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -10,7 +10,7 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
 export const Carousel = () => {
-  const [stories, setStories] = useState([]);
+  const [, setStories] = useState([]);
   const [rumorStories, setRumorStories] = useState([]);
   const [hearsayStories, setHearsayStories] = useState([]);
   const [historicalStories, setHistoricalStories] = useState([]);
@@ -18,17 +18,29 @@ export const Carousel = () => {
   const [activeHearsaySlide, setActiveHearsaySlide] = useState(0);
   const [activeHistoricalSlide, setActiveHistoricalSlide] = useState(0);
   const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  const apiUrl = import.meta.env.VITE_BACKEND_API || "http://localhost:3000";
+
+  // const handleRankUpdate = (updatedStory) => {
+  //   setStories((prevStories) =>
+  //     prevStories.map((story) =>
+  //       story._id === updatedStory._id ? updatedStory : story
+  //     )
+  //   );
+  // };
 
   const handleRankUpdate = (updatedStory) => {
-    setStories((prevStories) =>
-      prevStories.map((story) =>
+    const updateStateArray = (array) =>
+      array.map((story) =>
         story._id === updatedStory._id ? updatedStory : story
-      )
-    );
+      );
+
+    setStories((prev) => updateStateArray(prev));
+    setRumorStories((prev) => updateStateArray(prev));
+    setHearsayStories((prev) => updateStateArray(prev));
+    setHistoricalStories((prev) => updateStateArray(prev));
   };
 
   const updateStories = () => {
-    const apiUrl = import.meta.env.VITE_BACKEND_API || "http://localhost:3000";
     fetch(`${apiUrl}/stories`)
       .then((response) => response.json())
       .then((data) => {
@@ -148,13 +160,14 @@ export const Carousel = () => {
           modifier: 1,
           slideShadows: true,
         }}
-        pagination={{ clickable: true }}>
+        // pagination={{ clickable: true }}
+      >
         {rumorStories.map((story, index) => (
           <SwiperSlide key={story._id}>
             <StoryCard
               story={story}
               isActive={index === activeRumorSlide}
-              onRankUpdate={handleRankUpdate}
+              handleRankUpdate={handleRankUpdate}
               onUpdateStories={updateStories}
             />
           </SwiperSlide>
@@ -176,13 +189,14 @@ export const Carousel = () => {
           modifier: 1,
           slideShadows: true,
         }}
-        pagination={{ clickable: true }}>
+        // pagination={{ clickable: true }}
+      >
         {hearsayStories.map((story, index) => (
           <SwiperSlide key={story._id}>
             <StoryCard
               story={story}
               isActive={index === activeHearsaySlide}
-              onRankUpdate={handleRankUpdate}
+              handleRankUpdate={handleRankUpdate}
               onUpdateStories={updateStories}
             />
           </SwiperSlide>
@@ -205,13 +219,14 @@ export const Carousel = () => {
           modifier: 1,
           slideShadows: true,
         }}
-        pagination={{ clickable: true }}>
+        // pagination={{ clickable: true }}
+      >
         {historicalStories.map((story, index) => (
           <SwiperSlide key={story._id}>
             <StoryCard
               story={story}
               isActive={index === activeHistoricalSlide}
-              onRankUpdate={handleRankUpdate}
+              handleRankUpdate={handleRankUpdate}
               onUpdateStories={updateStories}
             />
           </SwiperSlide>
--- frontend/src/components/Storycard/Storycard.jsx
+++ frontend/src/components/Storycard/Storycard.jsx
@@ -3,7 +3,9 @@ import "./Storycard.css";
 import { timeSince } from "../utils/timeUtils";
 import likeIcon from "../../assets/like.svg";
 
-export const StoryCard = ({ story, isActive, onUpdateStories }) => {
+export const StoryCard = ({ story, isActive, handleRankUpdate }) => {
+  const apiUrl = import.meta.env.VITE_BACKEND_API || "http://localhost:3000";
+
   const cardStyle = {
     transform: isActive ? "scale(1)" : "scale(1)",
     transition: "transform 0.3s ease-in-out",
@@ -21,9 +23,9 @@ export const StoryCard = ({ story, isActive, onUpdateStories }) => {
       },
     })
       .then((response) => response.json())
-      .then(() => {
-        if (onUpdateStories) {
-          onUpdateStories(); // Function to trigger re-fetching of stories
+      .then((updatedStory) => {
+        if (handleRankUpdate) {
+          handleRankUpdate(updatedStory);
         }
       })
       .catch((error) => console.error("Error updating story ranking:", error));
@@ -41,8 +43,10 @@ export const StoryCard = ({ story, isActive, onUpdateStories }) => {
       </div>
       {isActive && (
         <div className="story-overlay">
-          <h3>{story.title}</h3>
-          <p>{story.content}</p>
+          <div className="story-overlay-content">
+            <h3>{story.title}</h3>
+            <p>{story.content}</p>
+          </div>
           <div className="overlay-date-icon">
             {timeSince(story.createdAt)}
             <button onClick={handleLikeClick} className="like-button">
@@ -68,5 +72,5 @@ StoryCard.propTypes = {
     image: PropTypes.string, // Image is optional for now
   }).isRequired,
   isActive: PropTypes.bool,
-  onRankUpdate: PropTypes.func,
+  handleRankUpdate: PropTypes.func,
 };
